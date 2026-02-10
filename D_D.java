7b2d3a9c2408731ed0469b3a30e2c1b9af3a53b9
Jonathan28x03x1999 <preissjonathan280399@icloud.com>
2026-02-10 11:20:54 +0100

    points

---
+++ D_D.java
@@ -0,0 +1,24 @@
+public class D_D {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO THIS DUMD ADVENTURE)";
+    }
+}
+    import java.util.Scanner;
+
+    Public class D_D {
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+         / Scanner erstellen
+
+            System.out.print(WHAT`S YOUR NAME DOUCHEBAG?);// ASKS 
+        THE PLAYER
+            String name = scanner.nextLIne();   //
+            speichert die Eingabe
+
+            System.out.print("HELLO" + name + "!");//
+            Begrüßung
+
+            scanner.close();// Scanner schließen
+          }
+         }
